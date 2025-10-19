531a97ff443307e5a76820f964fb349fad8f1e80
xXxDanya2007xXx <danya.ecorp.mail@gmail.com>
2025-10-19 20:34:35 +0500

    Add files for Programming (Python) homework

---
+++ Programming (Python)/15.10.2025/15-10-2025_ex-1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def find_digit():
+    string = input('Введите строку чисел: ')
+    N = int(input('Введите число N: '))
+    print(string[N])
+
+
+if __name__ == "__main__":
+    find_digit()
