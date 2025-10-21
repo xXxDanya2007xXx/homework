4de0c1892a54fea994c38cc802eccd83da1876e3
xXxDanya2007xXx <danya.ecorp.mail@gmail.com>
2025-10-21 21:41:30 +0500

    Add file `15.10.2025/15_10_2025-ex_2.py`

---
+++ Programming (Python)/15.10.2025/15_10_2025-ex_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def check_brackets(expression: str):
+    stack = []
+
+    for ch in expression:
+        if ch == '(':
+            stack.append(ch)
+        elif ch == ')':
+            if not stack:
+                return False
+            stack.pop()
+
+    return not stack
+
+
+if __name__ == "__main__":
+    expression = input('Введите выражение: ')
+    print(check_brackets(expression))
