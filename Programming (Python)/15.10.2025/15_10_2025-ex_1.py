1d6b0c52a6c348d0b58ca49edafda1b8281c0f0a
xXxDanya2007xXx <danya.ecorp.mail@gmail.com>
2025-10-21 21:48:45 +0500

    Update files for Programming (Python) homework

--- Programming (Python)/15.10.2025/15_10_2025-ex_1.py
+++ Programming (Python)/15.10.2025/15_10_2025-ex_1.py
@@ -5,15 +5,7 @@
 #     n = int(input('Введите число N: '))
 #     print(string[n])
 
-def find_digit():
-    while True:
-        try:
-            n = int(input('Введите число N: '))
-        except ValueError:
-            print('Err: Введите целое число.')
-            continue
-        break
-
+def find_digit(n: int):
     length = 1  # длина числа (1 для 1-9, 2 для 10-99, etc.)
     count = 9   # сколько чисел в группе
     start = 1   # начальное число группы
@@ -31,4 +23,12 @@ def find_digit():
 
 
 if __name__ == "__main__":
-    find_digit()
+    while True:
+        try:
+            n = int(input('Введите число N: '))
+        except ValueError:
+            print('Err: Введите целое число.')
+            continue
+        break
+
+    find_digit(n)
--- Programming (Python)/15.10.2025/15_10_2025-ex_2.py
+++ Programming (Python)/15.10.2025/15_10_2025-ex_2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-# Проверяем количество и порядок расставления скобок
 def check_brackets(expression: str):
     stack = []
 
@@ -12,9 +11,13 @@ def check_brackets(expression: str):
                 return False
             stack.pop()
 
-    return not stack
+    if not stack:
+        return 'Количество и положение скобок корректны.'
+    else:
+        return 'Количество и положение скобок некорректны.'
 
 
 if __name__ == "__main__":
     expression = input('Введите выражение: ')
+
     print(check_brackets(expression))
