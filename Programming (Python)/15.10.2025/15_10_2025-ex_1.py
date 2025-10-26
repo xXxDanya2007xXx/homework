eb75c54320a00d72d30690414f19240601f0b1d3
xXxDanya2007xXx <danya.ecorp.mail@gmail.com>
2025-10-26 18:59:28 +0500

    Update files

--- Programming (Python)/15.10.2025/15_10_2025-ex_1.py
+++ Programming (Python)/15.10.2025/15_10_2025-ex_1.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 
-# def find_digit():
-#     string = input("Введите строку чисел: ")
-#     n = int(input("Введите число N: "))
-#     print(string[n])
-
 def find_digit(n: int):
     length = 1  # длина числа (1 для 1-9, 2 для 10-99, etc.)
     count = 9   # сколько чисел в группе
